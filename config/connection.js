8830a1ee1e74f0e86f8c647473c0ed7aaa13c071
Nikki Vargas <nikkivargas@Nikkis-iMac.local>
2023-11-21 16:20:24 -0600

    push all code to fix sql on dell

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,21 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 module.exports = sequelize;
--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // * Id has: an interger dataType, false allowNull, true PrimaryKey, and true autoIncrement 
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:this.truncate
+    },
+    //Category_name has: a string datatype, and false allowNull
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
